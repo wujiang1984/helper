7837512366d7575c34511fac1e47abd960d4adb0
wujiang1984 <wujiang1984@aliyun.com>
2021-05-28 22:33:36 +0800

    first commit

---
+++ src/test/java/life/community/helper/HelperApplicationTests.java
@@ -0,0 +1,13 @@
+package life.community.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
